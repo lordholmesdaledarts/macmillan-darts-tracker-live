71afa65ff489d60ee9aafe80d0dad8b2c7ae8291
lordholmesdaledarts <lordholmesdaledarts@gmail.com>
2026-02-04 12:39:46 +0000

    Add Supabase client

---
+++ supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://schidmxklptsdpgedqae.supabase.co",
+  "sb_publishable_J2M0fJnO0PT0wexMJha_IQ_YHX-Hxnj"
+);
+
+export const EVENT_ID = "macmillan-2026";
