59d03b899f3e028bb337d66f2dc90106fd4d7e6a
lordholmesdaledarts <lordholmesdaledarts@gmail.com>
2026-02-04 13:59:01 +0000

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const CACHE = "macmillan-darts-v15";
+
 const ASSETS = [
   "./",
   "./index.html",
@@ -12,22 +13,33 @@ const ASSETS = [
   "./holmesdale.png"
 ];
 
-self.addEventListener("install", (event) => {
+// Install: cache everything and activate immediately
+self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("activate", (event) => {
+// Activate: clear ALL old caches and take control
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
     )
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+// Fetch: cache-first, fallback to network
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
